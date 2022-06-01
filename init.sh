c48f7357b3f5e2b132efc98d98bb59ba0cff27ab
Chris Lyons <chris@lyonsgroup.family>
2022-06-01 12:11:54 -0400

    initial commit

--- init.sh
+++ init.sh
@@ -6,6 +6,76 @@ set -x
 gunzip -f /tmp/pfSense-CE-memstick-ADI.img.gz
 cp /tmp/pfSense-CE-memstick-ADI.img /out
 
-ls -al /out
-ls -al /tmp
-echo "done!"
+#start pfsense vm to gather packages to build offline resources
+
+virsh pool-define-as "images" dir - - - - "/images"
+virsh pool-build "images"
+virsh pool-autostart "images"
+virsh pool-start "images"
+
+create_line="virt-install "
+create_line+="--hvm "
+create_line+="--virt-type=kvm "
+create_line+="--name=pfsense "
+create_line+="--memory=1000 "
+create_line+="--cpu=host-passthrough,cache.mode=passthrough "
+create_line+="--tpm emulator,model=tpm-tis,version=2.0 "
+create_line+="--memorybacking hugepages=yes "
+create_line+="--vcpus=8 "
+create_line+="--boot hd,menu=off,useserial=off "
+create_line+="--disk /tmp/pfSense-CE-memstick-ADI.img "
+create_line+="--disk pool=images,size=40,bus=virtio,sparse=no "
+create_line+="--connect qemu:///system "
+create_line+="--os-type=freebsd "
+create_line+="--serial tcp,host=0.0.0.0:4567,mode=bind,protocol=telnet "
+create_line+="--serial tcp,host=0.0.0.0:4568,mode=bind,protocol=telnet "
+create_line+="--network type=direct,source=ext-con,model=virtio,source_mode=bridge "
+create_line+="--network type=bridge,source=loc-static,model=virtio "
+create_line+="--network type=bridge,source=amp-net,model=virtio "
+create_line+="--os-variant=freebsd11.0 "
+create_line+="--graphics=vnc "
+
+create_line+="--channel unix,target.type=virtio,target.name='org.qemu.guest_agent.0' "
+
+create_line+="--autostart --wait 0"
+
+eval "$create_line"
+
+sleep 30;
+(echo open 127.0.0.1 4568;
+  sleep 60;
+  echo "ansi";
+  sleep 5;
+  echo 'A'
+  sleep 5;
+  echo -ne '\r\n';
+  sleep 5;
+  echo -ne '\r\n';
+  sleep 5;
+  echo -ne '\r\n';
+  sleep 5;
+  echo -ne '\r\n';
+  sleep 5;
+  echo -ne '\r\n';
+  sleep 5;
+  echo 'v';
+  echo ' ';
+  echo -ne '\r\n';
+  sleep 5;
+  echo 'Y'
+  sleep 160;
+  echo 'N';
+  sleep 5;
+  echo 'S';
+  sleep 5;
+) | telnet
+
+## remove install disk from pfsense
+virsh detach-disk --domain pfsense /tmp/pfSense-CE-memstick-ADI.img --persistent --config --live
+virsh reboot pfsense
+
+sleep 120;
+
+### cleanup
+runuser -l root -c  "rm -rf /tmp/usb"
+#####
