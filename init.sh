1f8894eca6a5571282a94c43f4e401971a02c5a5
Chris Lyons <chris@lyonsgroup.family>
2022-06-02 13:59:22 -0400

    initial commit

--- init.sh
+++ init.sh
@@ -14,15 +14,15 @@ gunzip -f /temp/pfSense-CE-memstick-ADI.img.gz
 startsector=$(file /temp/pfSense-CE-memstick-ADI.img | sed -n -e 's/.* startsector *\([0-9]*\),.*/\1/p')
 offset=$(($startsector * 512))
 
-rm -rf /tmp/usb
-mkdir /tmp/usb
+rm -rf /temp/usb
+mkdir /temp/usb
 runuser -l root -c  "mount -o loop,offset=$offset /temp/pfSense-CE-memstick-ADI.img /tmp/usb"
-rm -rf /tmp/usb/config.xml
-cp /openstack-pfsense.xml /tmp/usb
-mv /tmp/usb/openstack-pfsense.xml /tmp/usb/config.xml
+rm -rf /temp/usb/config.xml
+cp /openstack-pfsense.xml /temp/usb
+mv /temp/usb/openstack-pfsense.xml /temp/usb/config.xml
 
 ## generate OpenVPN TLS secret key
-runuser -l root -c  'openvpn --genkey --secret /tmp/openvpn-secret.key'
+runuser -l root -c  'openvpn --genkey --secret /temp/openvpn-secret.key'
 
 ### replace variables
 ## load generated cert variables
@@ -32,7 +32,7 @@ CA_CRT=$(cat </tmp/id_rsa.crt | base64 | tr -d '\n\r')
 INITIAL_WILDCARD_CRT=$(cat </tmp/wildcard.crt | base64 | tr -d '\n\r')
 INITIAL_WILDCARD_KEY=$(cat </tmp/wildcard.key | base64 | tr -d '\n\r')
 
-OPEN_VPN_TLS_KEY=$(cat </tmp/openvpn-secret.key | base64 | tr -d '\n\r')
+OPEN_VPN_TLS_KEY=$(cat </temp/openvpn-secret.key | base64 | tr -d '\n\r')
 #########
 
 ### cloudfoundry TCP ports
@@ -49,39 +49,39 @@ HOSTNAME="$ORGANIZATION-$HOSTNAME_SUFFIX"
 ###
 
 ##### replace PFSense template vars
-sed -i "s/{HOSTNAME}/$HOSTNAME/g" /tmp/usb/config.xml
-sed -i "s/{CF_TCP_START_PORT}/$CF_TCP_START_PORT/g" /tmp/usb/config.xml
-sed -i "s/{CF_TCP_END_PORT}/$CF_TCP_END_PORT/g" /tmp/usb/config.xml
-sed -i "s/{INTERNAL_VIP}/$INTERNAL_VIP/g" /tmp/usb/config.xml
-sed -i "s/{EXTERNAL_VIP}/$EXTERNAL_VIP/g" /tmp/usb/config.xml
-sed -i "s/{LAN_CENTOS_IP}/$LAN_CENTOS_IP/g" /tmp/usb/config.xml
-sed -i "s/{GATEWAY_ROUTER_IP}/$GATEWAY_ROUTER_IP/g" /tmp/usb/config.xml
-sed -i "s/{GATEWAY_ROUTER_DHCP_START}/$GATEWAY_ROUTER_DHCP_START/g" /tmp/usb/config.xml
-sed -i "s/{GATEWAY_ROUTER_DHCP_END}/$GATEWAY_ROUTER_DHCP_END/g" /tmp/usb/config.xml
-sed -i "s/{INTERNAL_DOMAIN_NAME}/$INTERNAL_DOMAIN_NAME/g" /tmp/usb/config.xml
-sed -i "s/{NETWORK_PREFIX}/$NETWORK_PREFIX/g" /tmp/usb/config.xml
-sed -i "s/{OPENVPN_CERT_PWD}/$(generate_random_pwd 31)/g" /tmp/usb/config.xml
-sed -i "s/{TELEGRAM_API}/$TELEGRAM_API/g" /tmp/usb/config.xml
-sed -i "s/{TELEGRAM_CHAT_ID}/$TELEGRAM_CHAT_ID/g" /tmp/usb/config.xml
-sed -i "s/{OINKMASTER}/$OINKMASTER/g" /tmp/usb/config.xml
-sed -i "s/{MAXMIND_KEY}/$MAXMIND_KEY/g" /tmp/usb/config.xml
-sed -i "s/{CA_CRT}/$CA_CRT/g" /tmp/usb/config.xml
-sed -i "s/{CA_KEY}/$CA_KEY/g" /tmp/usb/config.xml
-sed -i "s/{INITIAL_WILDCARD_CRT}/$INITIAL_WILDCARD_CRT/g" /tmp/usb/config.xml
-sed -i "s/{INITIAL_WILDCARD_KEY}/$INITIAL_WILDCARD_KEY/g" /tmp/usb/config.xml
-sed -i "s/{OPEN_VPN_TLS_KEY}/$OPEN_VPN_TLS_KEY/g" /tmp/usb/config.xml
-sed -i "s/{CLOUDFOUNDRY_VIP}/$CLOUDFOUNDRY_VIP/g" /tmp/usb/config.xml
-sed -i "s/{IDENTITY_VIP}/$IDENTITY_VIP/g" /tmp/usb/config.xml
-sed -i "s/{SUPPORT_VIP}/$SUPPORT_VIP/g" /tmp/usb/config.xml
-sed -i "s/{BASE_DN}/$(baseDN)/g" /tmp/usb/config.xml
-sed -i "s/{LB_ROUTER_IP}/$LB_ROUTER_IP/g" /tmp/usb/config.xml
-sed -i "s/{LB_DHCP_START}/$LB_DHCP_START/g" /tmp/usb/config.xml
-sed -i "s/{LB_DHCP_END}/$LB_DHCP_END/g" /tmp/usb/config.xml
-sed -i "s/{ADVANCED_BACKEND}/$ADVANCED_BACKEND/g" /tmp/usb/config.xml
-sed -i "s/{VPN_NETWORK}/$VPN_NETWORK/g" /tmp/usb/config.xml
+sed -i "s/{HOSTNAME}/$HOSTNAME/g" /temp/usb/config.xml
+sed -i "s/{CF_TCP_START_PORT}/$CF_TCP_START_PORT/g" /temp/usb/config.xml
+sed -i "s/{CF_TCP_END_PORT}/$CF_TCP_END_PORT/g" /temp/usb/config.xml
+sed -i "s/{INTERNAL_VIP}/$INTERNAL_VIP/g" /temp/usb/config.xml
+sed -i "s/{EXTERNAL_VIP}/$EXTERNAL_VIP/g" /temp/usb/config.xml
+sed -i "s/{LAN_CENTOS_IP}/$LAN_CENTOS_IP/g" /temp/usb/config.xml
+sed -i "s/{GATEWAY_ROUTER_IP}/$GATEWAY_ROUTER_IP/g" /temp/usb/config.xml
+sed -i "s/{GATEWAY_ROUTER_DHCP_START}/$GATEWAY_ROUTER_DHCP_START/g" /temp/usb/config.xml
+sed -i "s/{GATEWAY_ROUTER_DHCP_END}/$GATEWAY_ROUTER_DHCP_END/g" /temp/usb/config.xml
+sed -i "s/{INTERNAL_DOMAIN_NAME}/$INTERNAL_DOMAIN_NAME/g" /temp/usb/config.xml
+sed -i "s/{NETWORK_PREFIX}/$NETWORK_PREFIX/g" /temp/usb/config.xml
+sed -i "s/{OPENVPN_CERT_PWD}/$(generate_random_pwd 31)/g" /temp/usb/config.xml
+sed -i "s/{TELEGRAM_API}/$TELEGRAM_API/g" /temp/usb/config.xml
+sed -i "s/{TELEGRAM_CHAT_ID}/$TELEGRAM_CHAT_ID/g" /temp/usb/config.xml
+sed -i "s/{OINKMASTER}/$OINKMASTER/g" /temp/usb/config.xml
+sed -i "s/{MAXMIND_KEY}/$MAXMIND_KEY/g" /temp/usb/config.xml
+sed -i "s/{CA_CRT}/$CA_CRT/g" /temp/usb/config.xml
+sed -i "s/{CA_KEY}/$CA_KEY/g" /temp/usb/config.xml
+sed -i "s/{INITIAL_WILDCARD_CRT}/$INITIAL_WILDCARD_CRT/g" /temp/usb/config.xml
+sed -i "s/{INITIAL_WILDCARD_KEY}/$INITIAL_WILDCARD_KEY/g" /temp/usb/config.xml
+sed -i "s/{OPEN_VPN_TLS_KEY}/$OPEN_VPN_TLS_KEY/g" /temp/usb/config.xml
+sed -i "s/{CLOUDFOUNDRY_VIP}/$CLOUDFOUNDRY_VIP/g" /temp/usb/config.xml
+sed -i "s/{IDENTITY_VIP}/$IDENTITY_VIP/g" /temp/usb/config.xml
+sed -i "s/{SUPPORT_VIP}/$SUPPORT_VIP/g" /temp/usb/config.xml
+sed -i "s/{BASE_DN}/$(baseDN)/g" /temp/usb/config.xml
+sed -i "s/{LB_ROUTER_IP}/$LB_ROUTER_IP/g" /temp/usb/config.xml
+sed -i "s/{LB_DHCP_START}/$LB_DHCP_START/g" /temp/usb/config.xml
+sed -i "s/{LB_DHCP_END}/$LB_DHCP_END/g" /temp/usb/config.xml
+sed -i "s/{ADVANCED_BACKEND}/$ADVANCED_BACKEND/g" /temp/usb/config.xml
+sed -i "s/{VPN_NETWORK}/$VPN_NETWORK/g" /temp/usb/config.xml
 #######
 
-runuser -l root -c  'umount /tmp/usb'
+runuser -l root -c  'umount /temp/usb'
 
 #start pfsense vm to gather packages to build offline resources
 
@@ -146,7 +146,7 @@ virsh reboot pfsense
 sleep 120;
 
 ### cleanup
-runuser -l root -c  "rm -rf /tmp/usb"
+runuser -l root -c  "rm -rf /temp/usb"
 #####
 
 virsh destroy pfsense
