f6422586dbdda791c1bf29a3d0599a27cc787935
Chris Lyons <chris@lyonsgroup.family>
2022-07-06 19:43:31 -0400

    new buildserver setup

--- pf_functions.sh
+++ pf_functions.sh
@@ -20,20 +20,3 @@ install_pkg()
   yes | pkg install "$pkg_name"
   telegram_notify  "PFSense init: installed pkg -> $pkg_name"
 }
-
-parse_json()
-{
-  echo "$1" |
-    sed -e 's/[{}]/''/g' |
-    sed -e 's/", "/'\",\"'/g' |
-    sed -e 's/" ,"/'\",\"'/g' |
-    sed -e 's/" , "/'\",\"'/g' |
-    sed -e 's/","/'\"---SEPERATOR---\"'/g' |
-    awk -F=':' -v RS='---SEPERATOR---' "\$1~/\"$2\"/ {print}" |
-    sed -e "s/\"$2\"://" |
-    tr -d "\n\t" |
-    sed -e 's/\\"/"/g' |
-    sed -e 's/\\\\/\\/g' |
-    sed -e 's/^[ \t]*//g' |
-    sed -e 's/^"//' -e 's/"$//'
-}
