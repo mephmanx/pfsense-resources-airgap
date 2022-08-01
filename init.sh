a25f53a65d282911aeb09010a7ae50e785bfb55b
chris <chris@lyonsgroup.family>
2022-07-31 22:45:01 -0400

    cleanup and adding a keep vm arg

--- init.sh
+++ init.sh
@@ -209,7 +209,7 @@ if [ 'dev' == "$1" ]; then
   ### mount transfer img, copy file, detach and move to host
   virsh attach-disk pfsense --source /tmp/transfer.img --target vdc --persistent --config --live
 
-  sleep 600;
+  sleep 800;
   (echo open localhost 4568;
     sleep 30;
     echo 'cd /var/cache/pkg';
