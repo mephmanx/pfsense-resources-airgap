83a9291f9c1d437a98f248e574b5dd613d301469
chris <chris@lyonsgroup.family>
2022-08-05 15:57:20 -0400

    switching to pfsense pull cache

--- init.sh
+++ init.sh
@@ -254,6 +254,8 @@ EOF
   sleep 200;
   (echo open localhost 4568;
     sleep 30;
+    echo "\n";
+    sleep 10;
     echo "touch /root/pf-init-3.sh; touch /root/pf-init-3.sh.enc;";
     sleep 10;
     for element in "${pfsense_init_array[@]}"
