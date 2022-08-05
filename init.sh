45881c2e0a972536480b9d5d7b1b0a0dee28c723
chris <chris@lyonsgroup.family>
2022-08-05 14:44:21 -0400

    switching to pfsense pull cache

--- init.sh
+++ init.sh
@@ -297,9 +297,9 @@ pkg fetch -o /tmp/repo-dir -y qemu-guest-agent
 yes | pkg install bash
 sleep 200
 
-bash -c 'for col in $(cat /tmp/pkg-create-a.out | grep -B 1 missing | grep for | cut -d " " -f 4); do pkg fetch -r pfSense -o /tmp/repo-dir -y $col; mv /tmp/repo-dir/All/* /tmp/repo-dir; done;"
+bash -c 'for col in $(cat /tmp/pkg-create-a.out | grep -B 1 missing | grep for | cut -d " " -f 4); do pkg fetch -r pfSense -o /tmp/repo-dir -y $col; mv /tmp/repo-dir/All/* /tmp/repo-dir; done;'
 
-bash -c 'for col in $(cat /tmp/pkg-create-a.out | grep -B 1 "No such file or directory" | grep for | cut -d " " -f 4); do pkg fetch -r pfSense -o /tmp/repo-dir -y $col; mv /tmp/repo-dir/All/* /tmp/repo-dir; done;"
+bash -c 'for col in $(cat /tmp/pkg-create-a.out | grep -B 1 "No such file or directory" | grep for | cut -d " " -f 4); do pkg fetch -r pfSense -o /tmp/repo-dir -y $col; mv /tmp/repo-dir/All/* /tmp/repo-dir; done;'
 
 pkg repo -o /tmp/repo-dir /tmp/repo-dir
 tar cf /tmp/repo.tar ./*
