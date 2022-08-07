c1d852735aaffdbb63f808a3e9ab45ae0176f6d1
chris <chris@lyonsgroup.family>
2022-08-07 11:28:30 -0400

    switching to pfsense pull cache

--- init.sh
+++ init.sh
@@ -302,6 +302,9 @@ sleep 200
 
 bash -c 'for col in $(cat /tmp/pkg-create-a.out | grep -B 1 missing | grep for | cut -d " " -f 4); do pkg fetch -r pfSense -o /tmp/repo-dir -y $col; mv /tmp/repo-dir/All/* /tmp/repo-dir; done;'
 
+bash -c 'for col in $(cat /tmp/pkg-create-a.out | grep -B 1 missing | grep for | cut -d " " -f 4); do pkg fetch -r pfSense -o /tmp/repo-dir -y pfSense-pkg-$col; mv /tmp/repo-dir/All/* /tmp/repo-dir; done;'
+
+
 bash -c 'for col in $(cat /tmp/pkg-create-a.out | grep -B 1 "No such file or directory" | grep for | cut -d " " -f 4); do pkg fetch -r pfSense -o /tmp/repo-dir -y $col; mv /tmp/repo-dir/All/* /tmp/repo-dir; done;'
 
 bash -c 'for col in $(cat /tmp/pkg-create-a.out | grep -B 1 "No such file or directory" | grep for | cut -d " " -f 4); do pkg fetch -r pfSense -o /tmp/repo-dir -y pfSense-pkg-$col; mv /tmp/repo-dir/All/* /tmp/repo-dir; done;'
