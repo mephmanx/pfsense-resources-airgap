8d931f4c51aa63fa567b63452bc7f147bb65e9db
chris <chris@lyonsgroup.family>
2022-08-08 16:59:48 -0400

    switching to pfsense pull cache

--- pfsense-init.sh
+++ pfsense-init.sh
@@ -16,6 +16,6 @@ echo 'qemu_guest_agent_flags="-d -v -l /var/log/qemu-ga.log"' >> /etc/rc.conf
 
 service qemu-guest-agent start
 
-pw useradd squid
+yes | pkg install pfSense-pkg-squid
 rm -rf /root/pfsense-init.sh
 telegram_notify  "PFSense init: init complete!"
