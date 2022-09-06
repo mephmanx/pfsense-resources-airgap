fdf72beae362a49fda969afa824f488b41eb4c06
chris <chris@lyonsgroup.family>
2022-09-05 20:24:18 -0400

    pkgtouch error fix

--- init.sh
+++ init.sh
@@ -92,13 +92,6 @@ sed -i "s/{SUPPORT_HOST}/$SUPPORT_HOST/g" /temp/usb/config.xml
 sed -i "s/{IDENTITY_HOST}/$IDENTITY_HOST/g" /temp/usb/config.xml
 #######
 
-##### cert placeholders.  lengths are VERY important!!
-#sed -i "s/{CA_CRT}/$(generate_specific_pwd 2465)/g" /temp/usb/config.xml
-#sed -i "s/{CA_KEY}/$(generate_specific_pwd 4389)/g" /temp/usb/config.xml
-#sed -i "s/{INITIAL_WILDCARD_CRT}/$(generate_specific_pwd 2765)/g" /temp/usb/config.xml
-#sed -i "s/{INITIAL_WILDCARD_KEY}/$(generate_specific_pwd 4393)/g" /temp/usb/config.xml
-###
-
 runuser -l root -c  'umount /temp/usb'
 
 cp /temp/pfSense-CE-memstick-ADI.img /tmp/pfSense-CE-memstick-ADI-"$1".img
