533b6fcd226c220f969e01a0402d87f8dd33d2fc
chris <chris@lyonsgroup.family>
2022-09-10 16:41:03 -0400

    adding package cache

--- command-processor.sh
+++ command-processor.sh
@@ -2,7 +2,7 @@
 
 args=()
 for entry in "$@"; do
-  if grep -q ":" <<< "$entry"; then
+  if ! grep -q ":" <<< "$entry"; then
     echo "Arguments must be of the form <param name>:<param value>"
     exit 1
   fi
