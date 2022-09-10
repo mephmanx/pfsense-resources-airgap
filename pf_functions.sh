44a09a485f66fb4b6dcd16fbd8bce74e01893731
chris <chris@lyonsgroup.family>
2022-09-10 17:45:00 -0400

    adding package cache

--- pf_functions.sh
+++ pf_functions.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-telegram_notify()
-{
-#  token=$TELEGRAM_API
-#  chat_id=$TELEGRAM_CHAT_ID
-#  msg_text=$1
-#  curl -X POST  \
-#        -H 'Content-Type: application/json' -d "{\"chat_id\": \"$chat_id\", \"text\":\"$msg_text\", \"disable_notification\":false}"  \
-#        -s \
-#        "https://api.telegram.org/bot$token/sendMessage" > /dev/null
-}
-
 install_pkg()
 {
   pkg_name=$1
