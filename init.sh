139e2301d03ba11890c7ba816b503b199b47e738
chris <chris@lyonsgroup.family>
2022-09-11 19:48:42 -0400

    adding package cache

--- init.sh
+++ init.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-. /command-processor.sh "$@"
+args=()
+for entry in "$@"; do
+  if ! grep -q ":" <<< "$entry"; then
+    echo "Arguments must be of the form <param name>:<param value>"
+    exit 1
+  fi
+  IFS=':' read -ra line_entry <<<"$entry"
+  args+=("${line_entry[0]}:${line_entry[1]}")
+done
+
+for arg in "${args[@]}"; do
+  IFS=':' read -ra line_entry <<<"$arg"
+  bash -c "export ${line_entry[0]}=${line_entry[1]}"
+done
 
 exec 1>/out/pfsense-build-"$ENV".log 2>&1
 
